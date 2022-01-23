b815369c88ecb8e69552c236f0c8186776343dc3
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-01-23 13:57:31 +0100

    Basic auth token implementation
    https://developer.okta.com/blog/2019/02/04/create-and-verify-jwts-in-php

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-    require("php/database.php");
+    // require("php/database.php");
     
-    $database = new Database;
+    // $database = new Database;
+    // $a = $database->assocQuery("Select * FROM Users WHERE username = {0}", ["lajtkek"]);
 
-    $a = $database->assocQuery("Select * FROM Users WHERE username = {0}", ["lajtkek"]);
+    // foreach($a as $value){
+    //     echo $value["username"];
+    // }
+
+    require("php/authHelper.php");
+    $authHelper = new AuthHelper;
+    
+    //1702940400
+    $token = $authHelper->generateToken([
+        'user_id' => 1,
+        'role' => 'admin_kek',
+        'exp' => 1993828222
+    ]);
+    
+    echo $token;
     echo "<br>";
-    foreach($a as $value){
-        echo $value["username"];
-    }
+
+    print_r($authHelper->validateToken($token));
 ?>
---
+++ php/authHelper.php
@@ -0,0 +1,82 @@
+<?php 
+class AuthHelper {
+    //https://developer.okta.com/blog/2019/02/04/create-and-verify-jwts-in-php
+    private $secret = "7c32d31dbdd39f2111da0b1dea59e94f3ed715fd8cdf0ca3ecf354ca1a2e3e30";
+
+    public function __construct() {
+    }
+
+    public function __destruct(){
+    }
+
+    function base64UrlEncode($text)
+    {
+        return str_replace(
+            ['+', '/', '='],
+            ['-', '_', ''],
+            base64_encode($text)
+        );
+    }
+
+    public function generateToken($payload){
+        // Create the token header
+        $header = json_encode([
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ]);
+
+        // Create the token payload
+        $payload = json_encode($payload);
+
+        // Encode Header
+        $base64UrlHeader = $this->base64UrlEncode($header);
+
+        // Encode Payload
+        $base64UrlPayload = $this->base64UrlEncode($payload);
+
+        // Create Signature Hash
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true);
+
+        // Encode Signature to Base64Url String
+        $base64UrlSignature = $this->base64UrlEncode($signature);
+
+        // Create JWT
+        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+        return $jwt;
+    }
+
+    public function validateToken($token){
+        // split the token
+        $tokenParts = explode('.', $token);
+        $header = base64_decode($tokenParts[0]);
+        $payload = json_decode(base64_decode($tokenParts[1]));
+        $signatureProvided = $tokenParts[2];
+
+        // check the expiration time - note this will cause an error if there is no 'exp' claim in the token
+        $expiration = new DateTime("@$payload->exp");
+        $now = new DateTime();
+        $tokenExpired = $expiration < $now;
+
+        // build a signature based on the header and payload using the secret
+        $base64UrlHeader = $this->base64UrlEncode($header);
+        $base64UrlPayload = $this->base64UrlEncode(json_encode($payload));
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true);
+        $base64UrlSignature = $this->base64UrlEncode($signature);
+
+        // verify it matches the signature provided in the token
+        $signatureInvalid = !($base64UrlSignature === $signatureProvided);
+
+        if ($tokenExpired) {
+            return "expired";
+        }
+
+        if ($signatureInvalid) {
+            return "invalid_signature";
+        }
+
+        return json_encode($payload);
+    }
+}
+
+?>
