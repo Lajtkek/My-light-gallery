fb0c1e57bc638865f27961505ee86fb215f70067
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-01-23 11:49:21 +0100

    Basic database class
    -constructor
    -fetch assoc

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-henlo
+<?php
+    require("php/database.php");
+    
+    $database = new Database;
+
+    $a = $database->assocQuery("Select * FROM Users WHERE username = {0}", ["lajtkek"]);
+    echo "<br>";
+    foreach($a as $value){
+        echo $value["username"];
+    }
+?>
---
+++ php/database.php
@@ -0,0 +1,42 @@
+<?php 
+
+class Database {
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "light_gallery";
+    private $conn;
+    
+    function __construct() {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+
+        //echo "Connected successfully";
+    }
+
+    function assocQuery($sql, $parameters = []){
+        foreach($parameters as $key=>$param){
+            $escapedParam = "'" . mysqli_real_escape_string($this->conn, $param) . "'";
+            $sql = str_replace("{".$key."}", $escapedParam, $sql);
+        }
+
+        $result = $this->conn->query($sql);
+
+        $resultArray = [];
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                array_push($resultArray, $row);
+            }
+        }
+        return $resultArray;
+    }
+
+    public function __destruct(){
+        
+    }
+}
+
+?>
