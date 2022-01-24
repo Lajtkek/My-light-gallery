65ed56189ac709af618ae21ad9e8ea809e3992b2
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-01-24 19:35:58 +0100

    Added upload input for files

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -5,6 +5,20 @@
     <div class="upload">
       Upload
     </div>
+    <div class="upload-input-wrapper">
+      <v-file-input
+          v-model="files"
+          counter
+          multiple
+          show-size
+          truncate-length="20"
+      ></v-file-input>
+      <v-card-actions class="justify-center">
+        <v-btn color="blue" @click="upload">
+            Review and upload
+        </v-btn>
+      </v-card-actions>
+    </div>
   </div>
 </template>
 
@@ -17,7 +31,28 @@
     components: {
       Navbar,
     },
+    data: function () {
+      return {
+        files: []
+      }
+    },
     methods: {
+      upload(){
+        this.files.forEach(file => {
+          console.log(file)
+          //this.getBase64(file);
+        });
+      },
+      // getBase64(file) {
+      //   var reader = new FileReader();
+      //   reader.readAsDataURL(file);
+      //   reader.onload = function () {
+      //     console.log(reader.result);
+      //   };
+      //   reader.onerror = function (error) {
+      //     console.log('Error: ', error);
+      //   };
+      // }
     },
     mounted(){
     }
@@ -27,11 +62,18 @@
 <style lang="less" scoped>
   .upload{
     width: 100%;
-    height: 60vh;
+    height: 30vh;
     display: flex;
     justify-content: center;
     align-items: center;
 
     font-size: 3rem;
+  }
+
+  .upload-input-wrapper{
+    width: 60%;
+    min-width: 300px;
+    max-width: 800px;
+    margin: auto;
   }
 </style>
