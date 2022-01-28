a29b610bca916bc3ff5718b5569925a5f418ad9a
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-01-28 17:12:26 +0100

    Basic gallery images preview

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,16 @@
     <div class="gallery">
       Gallery
     </div>
-    <div v-for="file in files" :key="file.idFile">
-      <!-- store the link for file in cfg -->
-      <img :src="`http://localhost/resources/${file.permalink}.${file.extension}`">
+    <div class="file-wrapper">
+      <div v-for="file in files" :key="file.idFile" class="file">
+        <!-- store the link for file in cfg -->
+        <div class="file-prev">
+          <img :src="`http://localhost/resources/${file.permalink}.${file.extension}`">
+        </div>
+        <div class="file-info">
+          {{ file.filename }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,5 +49,46 @@
     width: 100%;
     height: 20vh;
     font-size: 3rem;
+  }
+
+  .file-wrapper{
+    .super-flex;
+    flex-wrap: wrap;
+
+    .file{
+      margin: 20px;
+      min-width: calc(300px - 20px);
+      width: calc(20% - 20px);
+
+      background: rgba(194, 194, 194, 0.671);
+      border-radius: 10px;
+
+      float: left;
+      display:grid;
+      grid-template-rows: 300px 60px;
+      grid-template-areas: 
+      "file-prev"
+      "bottom-bar";
+      
+      .file-prev{
+        .super-flex;
+        grid-area: file-prev;
+
+        img{
+          max-width: calc(100% - 20px);
+          max-height: calc(100% - 20px);
+        }
+      }
+
+      .file-info{
+        width: 100%;
+        grid-area: bottom-bar;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        text-align: center;
+        padding: 20px;
+      }
+    }
   }
 </style>
