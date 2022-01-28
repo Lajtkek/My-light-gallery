4ba807a23e4a1bf06b59ced7ac9d12d8a26a87e2
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-01-29 00:56:59 +0100

    added color to tag grid

--- src/components/TagOverview.vue
+++ src/components/TagOverview.vue
@@ -1,61 +1,60 @@
 <template>
   <div>
     <v-data-table
-    :headers="headers"
-    :items="$store.state.fileTags"
-    :items-per-page="50"
+      :headers="headers"
+      :items="$store.state.fileTags"
+      :items-per-page="50"
     >
-    <template v-slot:top>
-      <v-toolbar
-        flat
-      >
-        <v-toolbar-title>File tags</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
-        <v-btn @click="createTag()">Create new tag</v-btn>
-      </v-toolbar>
-    </template>
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>File tags</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-btn @click="createTag()">Create new tag</v-btn>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.color="{ item }">
+        <v-chip :color="item.color" >
+          {{ item.color }}
+        </v-chip>
+      </template>
     </v-data-table>
     <TagEditor :tag="selectedTag"></TagEditor>
   </div>
 </template>
 
 <script>
-  import store from '../store/store.js'
-  import TagEditor from "../components/TagEditor.vue";
+import store from "../store/store.js";
+import TagEditor from "../components/TagEditor.vue";
 
-  export default {
-    name: 'TagOverview',
-    components: { TagEditor },
-    data: function(){
-      return {
-        headers: [
-          { text: 'ID', value: 'idTag' },
-          { text: 'Name', value: 'name' },
-          { text: 'Color', value: 'color' },
-          { text: 'Code', value: 'code' },
-        ],
-        tags: [],
-        selectedTag: null,
-      }
+export default {
+  name: "TagOverview",
+  components: { TagEditor },
+  data: function () {
+    return {
+      headers: [
+        { text: "ID", value: "idTag" },
+        { text: "Name", value: "name" },
+        { text: "Code", value: "code" },
+        { text: "Color", value: "color" },
+      ],
+      tags: [],
+      selectedTag: null,
+    };
+  },
+  mounted() {
+    store.commit("getFileTags");
+  },
+  methods: {
+    createTag() {
+      this.$store.commit("setEditedTag", {
+        name: "",
+        code: "",
+        color: "#FF9393",
+      });
     },
-    mounted(){
-      store.commit("getFileTags");
-    },
-    methods: {
-      createTag(){
-        this.$store.commit("setEditedTag", {
-          name: "",
-          code: "",
-          color: "#FF9393"
-        })
-      }
-    },
-  }
+  },
+};
 </script>
 
 <style lang="less" scoped>
