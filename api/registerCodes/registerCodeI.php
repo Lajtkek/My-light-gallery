eb76cd9d01df0f4e3609fa54077d13ac69c21da5
Matěj Lajtkep <matej.lajtkep@gmail.com>
2022-06-18 18:37:46 +0200

    register codes init

--- api/registerCodes/registerCodeI.php
+++ api/registerCodes/registerCodeI.php
@@ -7,18 +7,19 @@
     RequestHelper::getInstance()->setHeader();
     RequestHelper::getInstance()->checkMethod("POST");
 
-    $code = RequestHelper::getInstance()->getParam("idTag", true);
+    $code = RequestHelper::getInstance()->getParam("code", true);
     $user = AuthHelper::getInstance()->auth(["admin"]);
  
     Database::getInstance()->beginTransaction();
     try{
         $code_id = Database::getInstance()->insertQuery("INSERT INTO RegisterCodes (code, idCreator) VALUES ('{0}', '{1}')", [$code, $user->idUser]);
+
         Database::getInstance()->commitTransaction();
 
         RequestHelper::getInstance()->resolve($code_id);
 
     }catch(Exception $e){
-        RequestHelper::getInstance()->reject($e);
+        RequestHelper::getInstance()->reject();
         Database::getInstance()->rollbackTransaction();
     }
     
--- src/components/RegisterCodesOverview.vue
+++ src/components/RegisterCodesOverview.vue
@@ -6,57 +6,71 @@
           <v-toolbar-title>RegisterCodes</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
+          <v-btn @click="showCodeDialog()">Create new code</v-btn>
         </v-toolbar>
       </template>
     </v-data-table>
+    <v-dialog v-model="showDialog" max-width="500px">
+      <v-card>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="12" md="12">
+              <v-text-field v-model="code" label="Code"></v-text-field>
+              <v-btn @click="createCode()">Create new code</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-
 export default {
   name: "RegisterCodesOverview",
   data: function () {
     return {
-        headers: [
-            { text: "ID", value: "idRegisterCode" },
-            { text: "Code", value: "code" },
-            { text: "Used", value: "used" },
-        ],
-        codes: [],
+      showDialog: false,
+      code: "",
+      headers: [
+        { text: "ID", value: "idRegisterCode" },
+        { text: "Code", value: "code" },
+        { text: "Used", value: "used" },
+      ],
+      codes: [],
     };
   },
   async mounted() {
-    let result = await Vue.prototype.get("registerCodes/getRegisterCodes", {});
-    if (!result.error) {
-        this.codes = result.data;
-    }
+    this.reloadCodes();
   },
   methods: {
-    onUserIU(user) {
-      let index = this.users.findIndex((x) => x.idUser == user.idUser);
-      if (index === -1) index = this.users.length;
-      this.users = this.users.filter((x) => x.idUser != user.idUser);
-      this.users.splice(index, 0, user);
+    async reloadCodes() {
+        let result = await this.get("registerCodes/getRegisterCodes", {});
+        if (!result.error) {
+            this.codes = result.data;
+    }
     },
-    createUser() {
-      this.$store.commit("setEditedUser", {
-        username: "",
-        password: "",
-        email: "",
-        roles: [],
-        isApproved: true,
-      });
+    showCodeDialog() {
+      this.showDialog = true;
+      this.code = "";
     },
-    editUser(user) {
-      user = { ...user };
-      user.roles = user.roles?.map((x) => x.idRole) ?? [];
-      this.$store.commit("setEditedUser", user);
-    },
-    // deleteUser(){
+    async createCode() {
+        if(this.codes.some(x => x.code?.toString().toLowerCase() == this.code?.toString().toLowerCase())){
+          this.showErrorTooltip("Tento kód už existuje");
+          return;
+        }
+        this.showDialog = false;
+        const result = await this.post("registerCodes/registerCodeI", {
+          code: this.code
+        });
 
-    // }
+        if(result.error)
+          this.showErrorTooltip("Nepodařilo se vytvořit kód");
+        else{
+          this.showSuccessTooltip("Kód byl vytvořen");
+          this.reloadCodes();
+        }
+    }
   },
 };
 </script>
